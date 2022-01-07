9203b4a0ae5075ab4ff306b9f911028fe309135a
ZER <eric.creencia25@gmail.com>
2022-01-07 09:27:24 +0800

    AspnetControllers

--- app/Http/Controllers/AspnetUserController.php
+++ app/Http/Controllers/AspnetUserController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\AspnetUser;
 use App\Models\Component;
 use App\Models\Project;
+use App\Models\Proponent;
 use App\Models\ProjectActivity;
 use Illuminate\Http\Request;
 use Session;
 use App\Http\Controllers\AspnetUserController;
 use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\DB;
+use Webpatser\Uuid\Uuid;
+use App\Http\Controllers\View;
+
+use Illuminate\Support\Str;
 
 class AspnetUserController extends Controller
 {
@@ -21,7 +26,15 @@ class AspnetUserController extends Controller
      */
     public function index()
     {
-        return view('auth.login');
+        $NewGUID = Uuid::generate()->string;
+        $GUID = Str::upper($NewGUID);
+
+        return view('welcome', compact('GUID'));
+    }
+
+    public function login()
+    {
+         return view('auth.login');
     }
 
     public function loginUser(Request $req)
@@ -30,13 +43,32 @@ class AspnetUserController extends Controller
         ->where('aspnet_Membership.Password', '=', $req->password )
         ->leftJoin('aspnet_Membership', 'aspnet_Users.UserId', '=', 'aspnet_Membership.UserId')
         ->first();
-        
+            
+        $now = new \DateTime(); 
+        $GUID = Uuid::generate()->string;
+
         if($user){
             $req->session()->put('data', $user);
+
+            DB::table('aspnet_Users')
+              ->where('UserId','=', $user->UserId)
+              ->where('UserName', '=', $user->UserName)
+              ->update([
+                'LastActivityDate' => $now->format('Y-m-d H:i:s')
+            ]);
+
+            DB::table('aspnet_Membership')
+              ->where('UserId','=', $user->UserId)
+              ->update([
+                'LastLoginDate' => $now->format('Y-m-d H:i:s')
+            ]);
+
+
+
             if($user->UserRole == 'Evaluator'){
                 return redirect('default');
             }else{
-                return redirect('new_document');
+                return redirect('default');
             }
             
         }else{
@@ -48,8 +80,9 @@ class AspnetUserController extends Controller
     public function logoutUser(Request $req)
     {
         if (Session::has('data')) {
-            Session::pull('data');
-            return redirect('login');
+            Session::flush();
+
+            return redirect('welcome');
         }
     }
 
@@ -59,23 +92,51 @@ class AspnetUserController extends Controller
         $UserRole = $req['UserRole'];
         $UserOffice = $req['UserOffice'];
 
-        $projects = Project::select('ProjectActivity', 'Project.GUID', '=', 'ProjectActivity.ProjectGUID')
-        ->select('Project.Address AS Address', 'Project.Municipality  AS Municipality', 'Project.Province AS Province', 'ProjectActivity.Status', 'ProjectActivity.Details AS Remarks', 'Project.ProjectName', 'Project.Region  AS Region', 'ProjectActivity.RoutedTo', 'ProjectActivity.RoutedFrom', 'Project.GUID AS ProjectGUID', 'Project.Stage', 'ProjectActivity.CreatedDate')
+        $todate = date('Y-m-d H:i:s');
+        $tomorrow = date('Y-m-d', strtotime( $todate . " +1 days"));
+
+        $projects = Project::select(
+            'Project.Address AS Address', 
+            'Project.Municipality  AS Municipality', 
+            'Project.Province AS Province', 
+            'Project.Stage', 
+            'Project.ProjectName', 
+            'Project.Region AS Region', 
+            'Project.GUID AS ProjectGUID', 
+
+            'ProjectActivity.Status', 
+            'ProjectActivity.Details AS Remarks', 
+            'ProjectActivity.GUID AS ActivityGUID', 
+            'ProjectActivity.RoutedTo', 
+            'ProjectActivity.RoutedFrom', 
+            'ProjectActivity.CreatedDate'
+        )
+        ->Join('ProjectActivity', function ($join) {
+            $join->on('Project.GUID', '=', 'ProjectActivity.ProjectGUID');
+
+            $join->whereRaw('ProjectActivity.ID IN (select MAX(a2.ID) from ProjectActivity as a2 
+                join project as u2 on u2.GUID = a2.ProjectGUID group by u2.GUID)');
+        })
+
+        ->where('ProjectActivity.Status', '<>', "For Screening")
+        ->where('ProjectActivity.RoutedTo', '=', $UserName)
         ->where('Project.CreatedBy', '=', $UserName)
-            ->leftJoin('ProjectActivity', 'Project.GUID', '=', 'ProjectActivity.ProjectGUID')
-            ->get();
+        ->where('Project.UpdatedDate', '>=', '2019-01-01')
+        ->where('Project.UpdatedDate', '<=', $tomorrow)
+        ->groupBy('Project.GUID')
+        ->get();
 
         $project = $projects;
 
         return DataTables::of($project)
         ->addColumn('Details', function($project) use ($UserRole){
             if($project->Stage > 0){
-                $details = '<a class="text-uppercase" href="ProjectApp/'.$project->ProjectGUID.'">'. $project->ProjectName.'</a>';
+                $details = '<a class="text-uppercase" href="ProjectApp/'.$project->ProjectGUID.'/'.$project->ActivityGUID.'">'. $project->ProjectName.'</a>';
             }else{
                 if($UserRole != 'Evaluator'){
-                    $details = '<a class="text-uppercase" href="NewApplications/'.$project->ProjectGUID.'">'. $project->ProjectName.'</a>';
+                    $details = '<a class="text-uppercase pointer" onclick="NewDocument('. "'" .$project->ProjectGUID. "'".')">'. $project->ProjectName.'</a>';
                 }else{
-                    $details = '<a class="text-uppercase" href="NewApplications/'.$project->ProjectGUID.'">'. $project->ProjectName.'</a>';
+                    $details = '<a class="text-uppercase pointer" onclick="NewDocument('. "'" .$project->ProjectGUID. "'".')">'. $project->ProjectName.'</a>';
                 }
             }
             
@@ -83,10 +144,14 @@ class AspnetUserController extends Controller
             return $details;
         })
         ->addColumn('Status', function($project){
+
             $details = '<i style="color:slategray;">'. $project->Status.'</i>';
+            
+            
             return $details;
         })
         ->addColumn('Remarks', function($project){
+
             $date = date("F j, Y g:i a", strtotime($project->CreatedDate));
             $details = '<small>'. $project->Remarks.' - <i>'.$project->RoutedFrom .' on '. $date .'</i></small>';
             return $details;
@@ -95,4 +160,22 @@ class AspnetUserController extends Controller
         ->make(true);
     }
 
+    public function createNewGUID()
+    {   
+        $NewGUID = Uuid::generate()->string;
+        $GUID = Str::upper($NewGUID);
+
+        return $GUID;
+    }
+
+
+    public function getProponentInformation(Request $req)
+    {   
+        $ProponentGUID = $req['ProponentGUID'];
+
+        $proponent = Proponent::where('Proponent.GUID', '=', $ProponentGUID)
+        ->first();
+
+        return $proponent;
+    }
 }
